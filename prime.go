85166751ce6a3d245634ac4e6c37f68ccac9c126
pallat <yod.pallat@gmail.com>
2015-02-24 22:07:57 +0700

    test 2 got 2

--- prime.go
+++ prime.go
@@ -1,5 +1,8 @@
 package prime
 
 func prime(number int) string {
+  if number == 2 {
+    return "2"
+  }
   return "1"
 }
--- prime_test.go
+++ prime_test.go
@@ -10,3 +10,10 @@ func TestPrimeOneShouldBeOne(t *testing.T) {
     t.Error("It should be 1 but got ", factor)
   }
 }
+
+func TestPrimeTwoShouldBeTwo(t *testing.T) {
+  factor := prime(2)
+  if factor != "2" {
+    t.Error("It should be 2 but got ", factor)
+  }
+}
